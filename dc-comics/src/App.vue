9a0f6404f2468a062c64fa58823c0886b5930528
VictorEnache <victor22enache@gmail.com>
2022-05-03 16:44:37 +0200

    main component

--- dc-comics/src/App.vue
+++ dc-comics/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import Header from './components/SiteHeader.vue'
+import Header from '@/components/SiteHeader.vue';
+import Main from '@/components/site_main.vue';
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Main
   }
 }
 </script>
@@ -24,11 +27,11 @@ export default {
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
+/*   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 
 </style>
---
+++ dc-comics/src/components/site_main.vue
@@ -0,0 +1,30 @@
+<template>
+    <main id="site_main">
+        <div class="container">
+            CONTENT GOES HERE
+        </div>
+    </main>
+</template>
+
+<script>
+    export default{
+        name:'site_main'
+    }
+    
+</script>
+
+<style lang="scss">
+#site_main{
+    height: 90px;
+    background-color: black;
+    color: white;
+    
+    .container{
+        height: 100%;
+        width: 1080px;
+        margin: auto;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
