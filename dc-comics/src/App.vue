2d2e5a7409dc74d55b938ea623bdfcb89a245ee1
VictorEnache <victor22enache@gmail.com>
2022-05-03 16:19:57 +0200

    header component

--- dc-comics/src/App.vue
+++ dc-comics/src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <div id="app">
-    
+    <Header/>
   </div>
 </template>
 
 <script>
-
+import Header from './components/SiteHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,4 +30,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ dc-comics/src/components/SiteHeader.vue
@@ -0,0 +1,67 @@
+<template>
+    <header id="SiteHeader">
+        <div class="container">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+
+
+export default {
+    name: 'SiteHeader'
+}
+</script>
+
+<style lang="scss">
+ #SiteHeader{
+
+     .container{
+         margin: auto;
+         width: 1080px;
+         display: flex;
+         justify-content: space-between;
+
+         .logo{
+             width:80px;
+
+             img{
+                 width: 100%;
+                 object-fit: cover;
+                 display: block;
+             }
+         }
+
+         nav{
+             height:80px;
+             
+
+             ul{
+                 height: 100%;
+                 list-style: none;
+                 display: flex;
+                 column-gap: 1rem;
+                 align-items: center;
+                 
+             }
+         }
+     }
+ }
+</style>
